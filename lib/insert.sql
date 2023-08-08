4998c85d4ad905ea509732a01054cc7d804524cd
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-08-08 15:27:01 -0400

    Complete Lab

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, 'M', "Black", "Happy", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, 'M', "Brown", "Angry", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4, 'F', "Black", "Sad", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 2, 'M', "Blue", "Happy", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 3, 'F', "Green", "Angry", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 5, 'M', "Green", "Sad", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 3, 'F', "Yellow", "Angry", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'F', "White", "Happy", 1);
+
